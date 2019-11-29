accc4726bdf582d2f89d89d2d8160b4f8e6adc5b
callie scott <callie.scott@versett.com>
2019-11-29 14:39:32 -0500

    changes to routes, fixed getHouse, getHouseById calls

--- api/server.js
+++ api/server.js
@@ -1,29 +1,30 @@
-'use strict'
-
-const mongoose = require('mongoose')
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-const {
-  API_PORT, 
+const { 
+  API_PORT,
   MONGODB_CONFIG,
   MONGODB_URL,
-  MONGODB_DBNAME  } = require('./utils/constants');
+  MONGODB_DBNAME
+} = require('./utils/constants');
 
 const { router: houseRouter } = require('./routes/houseRouter');
 
-app.use(express.json())
-app.use('/houseRouter', houseRouter);
+//these two use are middleware
+app.use(express.json());
+app.use('/houses', houseRouter);
 
-mongoose
-  .connect(MONGODB_URL, MONGODB_CONFIG)
-  .then(async () => {
-    console.log(`Connected to database at ${MONGODB_URL}`);
+//connect to database & handle err
+mongoose.connect(`${MONGODB_URL}/${MONGODB_DBNAME}`, MONGODB_CONFIG)
+  .then( async() => {
+    console.log('Connected to database...');
     app.listen(API_PORT, () => {
-      console.log(`Server is running on PORT: ${API_PORT}`);
+      console.log(`Server running on port: ${API_PORT}`);
     })
   })
   .catch((err) => {
-    console.error(err);
+    console.log('Failed to connect to database...')
+    throw err
   })
--- api/services/houseService.js
+++ api/services/houseService.js
@@ -1,53 +1,21 @@
-const uuid = require('uuid/v4');
 
-const { model: House } =require('../models/houseModel');
+const { model: House } = require('../models/houseModel');
 
-const HOUSES = [
-  { 
-    id: uuid(),
-    listingPrice: 999000,
-    address: {
-      street: "1 Balmy Avenue",
-      city: "Toronto",
-      postalCode: "M5V2T8"
-    },
-    agentValue: 1200000,
-    rating: 8
-  },
-  {
-    id: uuid(),
-    listingPrice: 1800000,
-    address: {
-      street: "29 Treadway Blvd",
-      city: "East York",
-      postalCode: "M4E2H8"
-    },
-    agentValue: 1600000,
-    rating: 4
+exports.listHouses = async () => {
+  try {
+    const houses = await House.find({});
+    return houses.map((house) => {
+      return house;
+    })
+  } catch (err) {
+    console.error(err);
+    throw err;
   }
-];
-
-exports.listHouses = async () => HOUSES.map((house) => {
-  const result = Object.assign({}, house);
-  return result;
-});
-
-// exports.listHouses = async () => {
-//   try {
-//     const houses = await houses.find({});
-//     return recipes.map((house) => {
-//       return house;
-//     })
-
-//   } catch (err) {
-//     console.error(err);
-//     throw err;
-//   }
-// }
+}
 
 exports.getHouse = async (houseId) => {
   try {
-    const house = await house.findById(houseId);
+    const house = await House.findById(houseId);
     return house;
   } catch (err) {
     console.error(err);
@@ -55,7 +23,8 @@ exports.getHouse = async (houseId) => {
   }
 };
 
-exports.createHouse = async (data = {}) => {
+exports.createHouse = async ( data = {} ) => {
+  //the below line will explode if something doesn't match the schema
   try {
     const house = new House(data);
     const doc = await house.save();
